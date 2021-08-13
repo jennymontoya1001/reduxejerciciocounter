8ce6b35405ee552e64fc579af80ae240646a39c1
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-08-12 20:12:20 -0500

    primer redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as action from './actions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    const count = useSelector(store => store.count);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h1>Counter:{count}</h1>
+            <button onClick={() => dispatch(action.increment())}>Increment</button>
+            <button onClick={() => dispatch(action.decrement())}>Decrement</button>
+            <button onClick={() => dispatch(action.reset())}>Reset</button>
+        </div>
+    )
 }
-
-export default App;
